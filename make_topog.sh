0ffacc15fb8847e9a510cfde4b6fd6a00de50508
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2020-09-01 14:18:19 +1000

    update topogtools submodule

--- make_topog.sh
+++ make_topog.sh
@@ -26,7 +26,7 @@ cd topogtools
 ./build.sh
 cd -
 
-./topogtools/editTopo.py --nogui --apply ocean_mask_edits.txt --output ocean_mask.nc ocean_mask_original.nc mask  # see https://github.com/COSIMA/access-om2/issues/210
+./topogtools/editTopo.py --overwrite --nogui --apply ocean_mask_edits.txt --output ocean_mask.nc ocean_mask_original.nc mask  # see https://github.com/COSIMA/access-om2/issues/210
 ./topogtools/float_vgrid  # this overwrites ocean_vgrid.nc
 # ./topogtools/gen_topo  # generates topog_new.nc; takes about 2 hours at 0.25 deg, so must be run via qsub of this script
 ./topogtools/deseas topog_new.nc topog_new_deseas.nc  # remove seas
